1e3adeb910711f4e66b8ee174609c8da874f374c
ariph007 <ariprayoga007@gmail.com>
2023-05-17 15:02:16 +0700

    Add process and factory for handling asset transfer

---
+++ src/com/rise/factories/ProcessFactories.java
@@ -0,0 +1,19 @@
+package com.rise.factories;
+
+import org.adempiere.base.IProcessFactory;
+import org.compiere.process.ProcessCall;
+
+import com.rise.process.AssetTransfer;
+
+public class ProcessFactories implements IProcessFactory{
+
+	@Override
+	public ProcessCall newProcessInstance(String className) {
+		
+		if(className.equals("com.rise.process.AssetTransfer"))
+			return new AssetTransfer();
+		
+		return null;
+	}
+
+}
